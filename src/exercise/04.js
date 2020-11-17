330174ac34e7b864ff9be5f60748acdf95d2875e
David Hargitai <hargitai.david@e-vista.hu>
2020-11-17 12:41:27 +0100

    4. Use callAll to compose functions

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -4,19 +4,22 @@
 import * as React from 'react'
 import {Switch} from '../switch'
 
+function callAll(...fns) {
+  return (...args) => {
+    fns.forEach(fn => {
+      fn && fn(...args)
+    })
+  }
+}
+
 function useToggle() {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
 
-  const getTogglerProps = props => ({
+  const getTogglerProps = ({onClick, ...props} = {}) => ({
     'aria-pressed': on,
+    onClick: callAll(onClick, toggle),
     ...props,
-    onClick: () => {
-      toggle()
-      if (props.onClick) {
-        props.onClick()
-      }
-    },
   })
 
   return {on, toggle, getTogglerProps}
